cf40d994c623a78226acb80a30b43dc733a98519
Alex <144359506+Alex-Alves1@users.noreply.github.com>
2025-10-27 18:09:00 -0600

    Remove run_length_encoding.h header file

--- run_length_encoding.h
+++ run_length_encoding.h
@@ -1,10 +1,1 @@
-#include <stddef.h>
 
-#ifndef RUN_LENGTH_ENCODING_H
-#define RUN_LENGTH_ENCODING_H
-
-size_t byte_compress(unsigned char *buff_ptr, size_t buff_size);
-
-size_t byte_decompress(unsigned char *buff_ptr, size_t buff_size, size_t buff_capacity);
-
-#endif
